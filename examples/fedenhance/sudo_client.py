d10f7fca0fdb8ed64c13ada32715d4173a67e911
dbisk <16860792+dbisk@users.noreply.github.com>
2022-08-05 15:49:08 -0700

    more work on fedenhance example

--- examples/fedenhance/sudo_client.py
+++ examples/fedenhance/sudo_client.py
@@ -13,10 +13,13 @@ Efficient Networks for Universal Audio Source Separation". MLSP 2020.
 import argparse
 
 import torch
+from tqdm import tqdm
 
+import asteroid_sdr as asteroid_sdr_lib
+import mixture_consistency
 import floes.client
 from groupcomm_sudormrf_v2 import GroupCommSudoRmRf
-from sudo_common import evaluate_model
+from sudo_common import * # PEP8: bad style
 
 
 class SuDOClient(floes.client.PyTorchClient):
@@ -27,31 +30,104 @@ class SuDOClient(floes.client.PyTorchClient):
     def set_device(self, device):
         self.device = device
     
-    def train(self):
+    def fake_train_test(self):
+        self.model = fake_train_one_sample(self.model, self.device)
+    
+    def train(self, **kwargs):
         """
-        Fake training using random data.
+        Training loop for one epoch of training.
         """
-        # load random data as the dataset
-        dummy_inputs = torch.rand(1, 1, 8000)
-        dummy_targets = torch.rand(1, 2, 8000)
+        # parse keyword arguments
+        if 'lr' not in kwargs:
+            kwargs['lr'] = 0.005
+        if 'dataloader' not in kwargs:
+            raise NameError(f"'dataloader' not in kwargs for train().")
+        
+        # create local optimizer
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=kwargs['lr'])
 
-        # define a totally fake loss function & optimizer
-        criterion = lambda x, y: torch.mean(torch.abs(x - y))
-        optimizer = torch.optim.Adam(self.model.parameters())
+        # loop through the data
+        with tqdm(kwargs['dataloader']) as pbar:
+            for cnt, data in enumerate(pbar):
+                optimizer.zero_grad()
 
-        device = self.device
-        dummy_inputs = dummy_inputs.to(device)
-        dummy_targets = dummy_targets.to(device)
-        self.model = self.model.to(device)
+                # data format:
+                # <speech> (batch, time_samples),
+                # <noise> (batch, time_samples)
+                input_active_speakers, noise_wavs, extra_noise_waves = data
+                input_active_speakers = input_active_speakers.unsqueeze(1)
+                input_noises = torch.stack([noise_wavs, extra_noise_waves], 1)
 
-        self.model.train()
+                # create a mask for zeroing out the second noise
+                zero_out_mask = (
+                    torch.rand([kwargs['bs'], 1]) > kwargs['p_single_mix']
+                ).to(torch.float32)
 
-        # backwards pass
-        optimizer.zero_grad()
-        estimated_sources = self.model(dummy_inputs)
-        loss = criterion(estimated_sources, dummy_targets)
-        loss.backward()
-        optimizer.step()
+                # zero out mixture
+                # equal probability to zero out a noise mixture or the mixture
+                # also containing the speaker
+                if (torch.rand([1]) < 0.5).item():
+                    input_noises[:, 1] = input_noises[:, 1] * zero_out_mask
+                else:
+                    input_noises[:, 0] = input_noises[:, 0] * zero_out_mask
+                    input_active_speakers[:, 0] = input_active_speakers[:, 0] * zero_out_mask
+                
+                # send to gpu
+                input_active_speakers = input_active_speakers.to(self.device)
+                input_noises = input_noises.to(self.device)
+
+                # create mixture of mixtures
+                input_mom = input_active_speakers.sum(1, keepdim=True) + \
+                            input_noises.sum(1, keepdim=True)
+                input_mom = input_mom.to(self.device)
+                input_mix_std = input_mom.std(-1, keepdim=True)
+                input_mix_mean = input_mom.mean(-1, keepdim=True)
+                input_mom = (input_mom - input_mix_mean) / (input_mix_std + 1e-9)
+
+                # forward
+                rec_sources_wavs = self.model(input_mom)
+                rec_sources_wavs = mixture_consistency.apply(
+                    rec_sources_wavs, input_mom
+                )
+
+                # if the node is supervised then use the appropriate loss.
+                if kwargs['is_supervised']:
+                    loss_fn = sup_sisdr(
+                        rec_sources_wavs,
+                        input_active_speakers,
+                        input_noises,
+                        input_mom,
+                        use_activity_masks=False
+                    )
+                else:
+                    loss_fn = unsup_mixit(
+                        rec_sources_wavs,
+                        input_active_speakers,
+                        input_noises,
+                        input_mom
+                    )
+                
+                # backward
+                loss_fn.backward()
+                if 'clip_grad_norm' in kwargs:
+                    if kwargs['clip_grad_norm'] > 0:
+                        torch.nn.utils.clip_grad_norm_(
+                            self.model.parameters(),
+                            kwargs['clip_grad_norm']
+                        )
+                
+                # optimize
+                optimizer.step()
+
+                # statistics
+                local_loss_sum = loss_fn.detach().item()
+
+                if cnt % 10 == 0:
+                    # update pbar
+                    pbar.set_description_str(
+                        f'Avg loss: {local_loss_sum / (cnt + 1)}.'
+                    )
+        return
 
 
 def main(args):
@@ -64,10 +140,22 @@ def main(args):
     # set address information
     address = args.address
 
+    # set the training arguments
+    dataloader = None # TODO
+
     # start the GRPC connection and client loop
     # this will continue until server indicates it is done
     print("Awaiting signal from server to begin")
-    trained_model = floes.client.start_client(client, address)
+    trained_model = floes.client.start_client(
+        client, address,
+        # additional training arguments TODO
+        dataloader=dataloader,
+        lr=0.001,
+        clip_grad_norm=0.0,
+        is_supervised=False,
+        bs=4,
+        p_single_mix=0.5
+    )
 
     # for metrics, just print them
     print("Server indicates training done. Evaluating new model...")
--- examples/fedenhance/sudo_common.py
+++ examples/fedenhance/sudo_common.py
@@ -6,9 +6,12 @@ sudo_common.py - Common functions between the SuDO-RM-RF clients and server.
 @org University of Illinois, Urbana-Champaign Audio Group
 """
 
+import copy
 from typing import Dict
 
 import torch
+
+import asteroid_sdr as asteroid_sdr_lib
 
 
 def evaluate_model(model: torch.nn.Module) -> Dict:
@@ -29,3 +32,149 @@ def evaluate_model(model: torch.nn.Module) -> Dict:
     estimated_sources = model(random_input)
     out_shape = estimated_sources.shape
     return {'output_shape': out_shape}
+
+
+def fake_train_one_sample(model: torch.nn.Module, device = 'cpu'):
+    """
+    Sanity check test function that makes sure that the model can perform a
+    backward pass with an arbitrary loss function and random (properly sized)
+    data.
+
+    Args:
+        model: `torch.nn.Module`
+            The model that will be "trained".
+        device: `torch.device`
+            The device to train on.
+    Returns:
+        torch.nn.Module
+            The model "trained" on one data sample.
+    """
+    # load random data as the dataset
+    dummy_inputs = torch.rand(1, 1, 8000)
+    dummy_targets = torch.rand(1, 2, 8000)
+
+    # define a totally fake loss function & optimizer
+    criterion = lambda x, y: torch.mean(torch.abs(x - y))
+    optimizer = torch.optim.Adam(model.parameters())
+
+    dummy_inputs = dummy_inputs.to(device)
+    dummy_targets = dummy_targets.to(device)
+    model = model.to(device)
+    model.train()
+
+    # backwards pass
+    optimizer.zero_grad()
+    estimated_sources = model(dummy_inputs)
+    loss = criterion(estimated_sources, dummy_targets)
+    loss.backward()
+    optimizer.step()
+
+    return model
+
+
+def normalize_tensor_wav(wav_tensor, eps=1e-8, std=None):
+    mean = wav_tensor.mean(-1, keepdim=True)
+    if std is None:
+        std = wav_tensor.std(-1, keepdim=True)
+    return (wav_tensor - mean) / (std + eps)
+
+
+def snr_loss(est, ref, eps=1e-9):
+    error_element = est - ref
+    error = torch.sum(error_element**2, dim=-1)
+    power_ref = torch.sum(ref**2, dim=-1)
+    return 10. * torch.log10(error + eps + 0.001 * power_ref)
+
+
+def sup_sisdr(rec_sources_wavs, input_active_speakers, input_noises,
+              input_mom, use_activity_masks):
+    supervised_ast_loss = asteroid_sdr_lib.PITLossWrapper(
+        asteroid_sdr_lib.pairwise_neg_sisdr, pit_from='pw_mtx'
+    )
+    ref_speech = normalize_tensor_wav(input_active_speakers)
+    ref_noises = normalize_tensor_wav(input_noises)
+
+    if use_activity_masks:
+        ref_speech_powers = torch.sum(input_active_speakers ** 2, dim=-1,
+                                      keepdim=True)
+        input_mom_powers = torch.sum(input_mom ** 2, dim=-1, keepdim=True)
+        mixtures_input_snr = 10. * torch.log10(
+            ref_speech_powers / (input_mom_powers + 1e-9))
+        ref_speech_activity_mask = mixtures_input_snr.ge(0.001)
+
+        ref_noise_powers = torch.sum(input_noises ** 2, dim=-1,
+                                     keepdim=True)
+        mixtures_input_snr = 10. * torch.log10(
+            ref_noise_powers / (input_mom_powers + 1e-9))
+        ref_noise_activity_mask = mixtures_input_snr.ge(0.001)
+
+        speech_error = ref_speech_activity_mask * torch.clamp(
+            asteroid_sdr_lib.pairwise_neg_sisdr(
+                rec_sources_wavs[:, 0:1], ref_speech), min=-50., max=50.)
+
+        noise_error = ref_noise_activity_mask * torch.clamp(
+            supervised_ast_loss(
+                rec_sources_wavs[:, 1:], ref_noises), min=-50., max=50.)
+    else:
+        speech_error = torch.clamp(
+            asteroid_sdr_lib.pairwise_neg_sisdr(
+                rec_sources_wavs[:, 0:1], ref_speech), min=-50.,
+            max=50.)
+
+        noise_error = torch.clamp(
+            supervised_ast_loss(
+                rec_sources_wavs[:, 1:], ref_noises), min=-50., max=50.)
+
+    return speech_error.mean() + noise_error
+
+
+def unsup_mixit(rec_sources_wavs, input_active_speakers, input_noises,
+                input_mom):
+    ref_mix_1 = normalize_tensor_wav(
+        input_active_speakers + input_noises[:, 0:1])
+    ref_mix_2 = normalize_tensor_wav(input_noises[:, 1:2])
+
+    ref_mix1_powers = torch.sum(
+        (input_active_speakers + input_noises[:, 0:1]) ** 2,
+        dim=-1, keepdim=True)
+    input_mom_powers = torch.sum(input_mom ** 2, dim=-1, keepdim=True)
+    mixtures_input_snr = 10. * torch.log10(
+        ref_mix1_powers / (input_mom_powers + 1e-9))
+    ref_mix1_activity_mask = mixtures_input_snr.ge(0.001)
+
+    ref_mix2_powers = torch.sum(input_noises[:, 1:2]**2, dim=-1, keepdim=True)
+    mixtures_input_snr = 10. * torch.log10(
+        ref_mix2_powers / (input_mom_powers + 1e-9))
+    ref_mix2_activity_mask = mixtures_input_snr.ge(0.001)
+
+    er_00 = ref_mix1_activity_mask * torch.clamp(
+        asteroid_sdr_lib.pairwise_neg_sisdr(
+            rec_sources_wavs[:, 0:1] + rec_sources_wavs[:, 1:2], ref_mix_1),
+        min=-50., max=50.)
+    er_01 = ref_mix2_activity_mask * torch.clamp(
+        asteroid_sdr_lib.pairwise_neg_sisdr(
+            rec_sources_wavs[:, 2:3], ref_mix_2), min=-50., max=50.)
+
+    er_10 = ref_mix1_activity_mask * torch.clamp(
+        asteroid_sdr_lib.pairwise_neg_sisdr(
+            rec_sources_wavs[:, 0:1] + rec_sources_wavs[:, 2:3], ref_mix_1),
+        min=-50., max=50.)
+    er_11 = ref_mix2_activity_mask * torch.clamp(
+        asteroid_sdr_lib.pairwise_neg_sisdr(
+            rec_sources_wavs[:, 1:2], ref_mix_2), min=-50., max=50.)
+
+    errors = torch.cat([er_00 + er_01,
+                        er_10 + er_11], 1)
+    return torch.mean(torch.min(errors, 1)[0])
+
+
+def average_weights(w):
+    """
+    Returns the average of the weights.
+    """
+    w_avg = copy.deepcopy(w[0])
+    for key in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[key] += w[i][key]
+        w_avg[key] = torch.div(w_avg[key], len(w))
+    return w_avg
