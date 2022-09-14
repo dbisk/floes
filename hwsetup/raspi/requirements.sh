f699742d72082be2134803b03fa3bbcc53dd4970
dbisk <16860792+dbisk@users.noreply.github.com>
2022-09-13 21:14:05 -0700

    fix requirements for raspis

--- hwsetup/raspi/requirements.sh
+++ hwsetup/raspi/requirements.sh
@@ -6,12 +6,10 @@ sudo apt upgrade -y
 sudo apt install -y git vim
 sudo apt install -y python3-pip python3-venv libopenblas-dev libopenmpi-dev libomp-dev
 
-# install the virtual environment for torch
-python3 -m venv .venv
-source .venv/bin/activate
-pip install setuptools==58.3.0
-pip install Cython
-pip install torch
+# install torch (note: installs system wide. only do this on edge platforms.)
+sudo -H pip3 install setuptools==58.3.0
+sudo -H pip3 install Cython
+sudo -H pip3 install torch
 
 # install floes (which installs grpc)
-pip install floes-0.0.3-py3-none-any.whl
+sudo -H pip3 install floes-0.0.3-py3-none-any.whl
